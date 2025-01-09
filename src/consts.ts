3b76d5c388b169c5d74ff015f9d18b2082264e21
Edgelounger <edge@edgelounger.com>
2025-01-08 19:17:19 -0500

    refactor: update branding and metadata for Edgelounger theme

--- src/consts.ts
+++ src/consts.ts
@@ -2,43 +2,43 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 // Base Page Metadata, src/layouts/BaseLayout.astro
-export const BRAND_NAME = "AstroVerse";
-export const SITE_TITLE = "AstroVerse";
-export const SITE_DESCRIPTION = "An Astro Theme for Visual Storytelling";
+export const BRAND_NAME = "Edgelounger";
+export const SITE_TITLE = "Edgelounger";
+export const SITE_DESCRIPTION = "Lounging on the edge of the web";
 
 // Tags Page Metadata, src/pages/tags/index.astro
-export const Tags_TITLE = "AstroVerse - All Tags";
+export const Tags_TITLE = "Edgelounger - All Tags";
 export const Tags_DESCRIPTION =
-  "AstroVerse - All tags and the count of articles related to each tag";
+  "Edgelounger - All tags and the count of articles related to each tag";
 
 // Tags Page Metadata, src/pages/tags/[tag]/[page].astro
 export function getTagMetadata(tag: string) {
   return {
-    title: `All articles on '${tag}' tag in AstroVerse`,
-    description: `Explore articles about ${tag} for different perspectives and in-depth analysis.`,
+    title: `All articles on '${tag}' tag in Edgelandia`,
+    description: `Explore articles about ${tag} for different perspectives.`,
   };
 }
 
 // Category Page Metadata, src/pages/category/[category]/[page].astro
 export function getCategoryMetadata(category: string) {
   return {
-    title: `All articles in '${category}' category in AstroVerse`,
-    description: `Browse all articles under the ${category} category in AstroVerse`,
+    title: `All articles in '${category}' category in Edgelandia`,
+    description: `Browse all articles under the ${category} category in Edgelandia`,
   };
 }
 
 // Header Links, src/components/Header.astro
 export const HeaderLinks = [
-  { href: "/category/One/1/", title: "One" },
-  { href: "/category/Two/1/", title: "Two" },
-  { href: "/category/Three/1/", title: "Three" },
+  { href: "/category/One/1/", title: "Earth" },
+  { href: "/category/Two/1/", title: "Nature" },
+  { href: "/category/Three/1/", title: "Human" },
 ];
 
 // Footer Links, src/components/Footer.astro
 export const FooterLinks = [
-  { href: "/posts/why-astro/", title: "Astro" },
-  { href: "/posts/tailwind-typography/", title: "Tailwind" },
-  { href: "/tags/", title: "Tags" },
+  { href: "/posts/why-astro/", title: "Edge" },
+  { href: "/posts/tailwind-typography/", title: "River" },
+  { href: "/tags/", title: "Sky" },
 ];
 
 // Social Links, src/components/Footer.astro
